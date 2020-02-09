0db5535d9a3d1cf680e35dde0e50a2d36425b4b8
Daniel Tsutomu Obara <daniel.tsutomu@hotmail.com>
2020-02-09 20:52:11 -0300

    Validation environment to return specific errors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,3 +1,5 @@
+import 'dotenv/config';
+
 import express, { json } from 'express';
 import Youch from 'youch';
 import * as Sentry from '@sentry/node';
@@ -30,8 +32,12 @@ class App {
 
   exceptionHandler() {
     this.server.use(async (err, req, res, next) => {
-      const errors = await new Youch(err, req).toJSON();
-      return res.status(500).json(errors);
+      if (process.env.NODE_ENV === 'development') {
+        const errors = await new Youch(err, req).toJSON();
+        return res.status(500).json(errors);
+      }
+
+      return res.status(500).json({ error: 'Internal server error' });
     });
   }
 }
